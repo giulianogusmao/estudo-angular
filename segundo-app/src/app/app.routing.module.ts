c0cf434991939550acc9ca1eaa0a7e7be7a71ef7
Giuliano Gusmão <gusmaoggg@gmail.com>
2017-08-14 13:34:42 -0300

    #71 - Usar ou não # (hash) na rota

--- segundo-app/src/app/app.routing.module.ts
+++ segundo-app/src/app/app.routing.module.ts
@@ -52,7 +52,7 @@ const appRoutes: Routes = [
 ]
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, { useHash: false })],
     exports: [RouterModule]
 })
 
